48728addf126ff989c82191d9e7aa05fb77196af
Maximiliano Luci <maximiliano.luci@icloud.com>
2022-11-18 16:46:55 +0100

    Creación de SWCodi

---
+++ P1SecureCode/CustomControls/SWCodi.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomControls
+{
+    public partial class SWCodi : UserControl
+    {
+        public SWCodi()
+        {
+            InitializeComponent();
+        }
+
+        private string _NomTaula;
+
+        public string NomTaula
+        {
+            get { return _NomTaula; }
+            set { _NomTaula = value; }
+        }
+
+        private string _Nomcodi;
+
+        public string Nomcodi
+        {
+            get { return _Nomcodi; }
+            set { _Nomcodi = value; }
+        }
+
+        private string _NomDesc;
+
+        public string NomDesc
+        {
+            get { return _NomDesc; }
+            set { _NomDesc = value; }
+        }
+
+        private string _NomId;
+
+        public string NomId
+        {
+            get { return _NomId; }
+            set { _NomId = value; }
+        }
+    }
+}
