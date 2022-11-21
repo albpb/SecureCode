7f2e226f43eada9ce6b7de6ad1da20af643500f7
Leandro Queiroz <leandroqros@gmail.com>
2022-11-21 20:34:00 +0100

    añadido form
    Formulario de cambio de contraseña añadido al proyeto

--- P1SecureCode/validacion/FormValidacion.cs
+++ P1SecureCode/validacion/FormValidacion.cs
@@ -16,63 +16,19 @@ namespace validacion
         {
             InitializeComponent();
         }
-        //boleanas de validacion
-        bool user = false, pass = false;
-        //consulta para verificar si existen
-        string consulta = "select * from Users where 1=1";
         
         private void bntLogin_Click(object sender, EventArgs e)
         {
-            string consultaDeValidacion = consulta + " and Login = '" + txtUser.Text + "' AND Password is not null";
-
-            //Validacion para usuario
-            if (txtUser.Text == "leandro")
+            //Contraseña Inicial
+            if (txtPass.Text == "12345aA")
             {
-                user = true;
-            }
-
-            //Validacion para pass
-            if (txtPass.Text == "senha")
-            {
-                pass = true;
-
-            }
-
-            //si usuario incorrecto
-            if (user.Equals(false) && pass)
-            {
-                MessageBox.Show("Usuario Incorrecto");
-            }
-
-            //si pass incorrecto
-            if (user && pass.Equals(false))
-            {
-                MessageBox.Show("Contraseña incorrecta");
-            }
-
-            //Puede acceder
-            if (user && pass)
-            {
-                //llamar pantalla principal
-            }
-            //················································································································································
-            //verificacion en ddbb:
-            if (user.Equals(false) || pass.Equals(false))
-            {
-                //cerrar programa
+                //Abrir form de actualizacion de password
             }
             else
             {
-                if (user && pass)
-                {
-                    //entrar programa
-                }
-                else
-                {
-
-                }
-                //Verificar primero login
+                //Abrir form de app principal
             }
+
         }
     }
 }
