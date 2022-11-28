efed88252309593426295a9e26c697b256fd4a7b
MC <manelcagigos@gmail.com>
2022-11-28 20:50:02 +0100

    mas cambios

--- P1SecureCode/P1SecureCode/Program.cs
+++ P1SecureCode/P1SecureCode/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using splash;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace P1SecureCode
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new splashscreen());
         }
     }
 }
